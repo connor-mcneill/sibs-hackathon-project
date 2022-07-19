7247d24e2120ef6771aedad5ac06d970a6f8ddae
LucyL0v0 <yliu268@ncsu.edu>
2022-07-19 19:35:29 -0400

    Update hackathon-data-cleaning.R

--- hackathon-data-cleaning.R
+++ hackathon-data-cleaning.R
@@ -87,7 +87,7 @@ summary(glm(REC_IM ~ AGE + STENOK_AN + endocr_01 + zab_leg_01 +
               GEPAR_S_n + TRENT_S_n, family='binomial', data=mi_comp_clean))
 
 
-#--------------------LASSO LOGISTIC REGRESSION----------------------------
+#--------------------LASSO/Elastic Net LOGISTIC REGRESSION----------------------------
 library(caret)
 library(glmnet)
 set.seed(123)
@@ -122,10 +122,27 @@ cv.lasso.b$lambda.1se #The most-regularized model within one standard error of t
 
 #Estimated Odds Ratios
 lasso.b.coef <- coef(cv.lasso.b, s=cv.lasso.b$lambda.min) #lambda.1se --> lambda.min b/c option 1 ends up with 0 variables.
-exp(lasso.b.coef)#Estimated Odds Ratios
-#~~~~~~~~~~~~~BRUH WHAT
+lasso.b.coef #########MANY DROPPED!
+#exp(lasso.b.coef) #Estimated Odds Ratios
+#MANY VARIABLE IMPACT NEGATIVELY TO RESPONSE VAR.
 
+#~~~~~~ROC LASSO Option 1~~~~~~~~
+#FIRST, look at the estimated probabilities, and then convert these into"TRUE" or "FALSE":
+phat <- predict(cv.lasso.b, newx=xTest[1:10,], s=cv.lasso.b$lambda.1se, type="response")
+yhat <- predict(cv.lasso.b, newx=xTest[1:10,], s=cv.lasso.b$lambda.1se, type="class")
+cbind(phat, yhat, yTest[1:10], unname(dTest[1:10,]))
 
+#SECOND,consider many ways to convert the estimated probabilities into TRUE or FALSE
+phat <- predict(cv.lasso.b, newx=xTest, s=cv.lasso.b$lambda.1se, type="response")
+#install.packages("ROCR")
+library(ROCR)
+perf <- ROCR::performance(ROCR::prediction(phat, yTest), "tpr", "fpr")
+par(las=1)
+plot(perf, colorize=TRUE, print.cutoffs.at=seq(0,1,by=.1), text.adj=c(-0.2,1.7)) #plot ROC curve
+
+#The ideal ROC curve has area-under-the-curve of one, so let's see how close we get to one:
+as.numeric( ROCR::performance(ROCR::prediction(phat, yTest), "auc")@y.values ) #AUC(ROC)
+#ANSWER: 0.5 --> STRAIGHT LINE.
 
 #############LASSO Reg for option2: Make med variables binary#############
 set.seed(777)
@@ -154,8 +171,27 @@ cv.lasso.b2$lambda.1se #The most-regularized model within one standard error of 
 
 #Estimated Odds Ratios
 lasso.b.coef2 <- coef(cv.lasso.b2, s=cv.lasso.b2$lambda.min) #lambda.1se --> lambda.min b/c option 1 ends up with 0 variables.
-exp(lasso.b.coef2)#Estimated Odds Ratios
-##~~~~~~~~~~LOOKS BETTER, 5 VARIABLE IS LITTLE LESS
+lasso.b.coef2 ######LESS ARE DROPPED THAN OPTION1
+#exp(lasso.b.coef2) #Estimated Odds Ratios
+#LOOKS BETTER, 5 VARIABLE IS LITTLE LESS
+
+#~~~~~~ROC LASSO Option 2~~~~~~~~
+#FIRST, look at the estimated probabilities, and then convert these into"TRUE" or "FALSE":
+phat <- predict(cv.lasso.b2, newx=xTest2[1:10,], s=cv.lasso.b2$lambda.1se, type="response")
+yhat <- predict(cv.lasso.b2, newx=xTest2[1:10,], s=cv.lasso.b2$lambda.1se, type="class")
+cbind(phat, yhat, yTest2[1:10], unname(dTest2[1:10,]))
+
+#SECOND,consider many ways to convert the estimated probabilities into TRUE or FALSE
+phat <- predict(cv.lasso.b2, newx=xTest2, s=cv.lasso.b2$lambda.1se, type="response")
+#install.packages("ROCR")
+library(ROCR)
+perf <- ROCR::performance(ROCR::prediction(phat, yTest2), "tpr", "fpr")
+par(las=1)
+plot(perf, colorize=TRUE, print.cutoffs.at=seq(0,1,by=.1), text.adj=c(-0.2,1.7)) #plot ROC curve
+
+#The ideal ROC curve has area-under-the-curve of one, so let's see how close we get to one:
+as.numeric( ROCR::performance(ROCR::prediction(phat, yTest2), "auc")@y.values ) #AUC(ROC)
+#ANSWER: 0.7223995
 
 ###############Elsatic Net Reg for option1: Reduce dimensionality of ordinal med variables############
 inTrain3 <- as.vector(createDataPartition(mi_comp_clean1[,40],p=0.8,list=FALSE, times=1))
@@ -183,9 +219,26 @@ cv.lasso.b3$lambda.1se #The most-regularized model within one standard error of 
 
 #Estimated Odds Ratios
 lasso.b.coef3 <- coef(cv.lasso.b3, s=cv.lasso.b3$lambda.1se)
-exp(lasso.b.coef3)#Estimated Odds Ratios
+lasso.b.coef3 ###########NO GOOD... EVERYTHING DROPPED.
+#exp(lasso.b.coef3) #Estimated Odds Ratios
 
+#~~~~~~ROC Elastic Net Option 1~~~~~~~~
+#FIRST, look at the estimated probabilities, and then convert these into"TRUE" or "FALSE":
+phat <- predict(cv.lasso.b3, newx=xTest3[1:10,], s=cv.lasso.b3$lambda.1se, type="response")
+yhat <- predict(cv.lasso.b3, newx=xTest3[1:10,], s=cv.lasso.b3$lambda.1se, type="class")
+cbind(phat, yhat, yTest3[1:10], unname(dTest3[1:10,]))
 
+#SECOND,consider many ways to convert the estimated probabilities into TRUE or FALSE
+phat <- predict(cv.lasso.b3, newx=xTest3, s=cv.lasso.b3$lambda.1se, type="response")
+#install.packages("ROCR")
+library(ROCR)
+perf <- ROCR::performance(ROCR::prediction(phat, yTest3), "tpr", "fpr")
+par(las=1)
+plot(perf, colorize=TRUE, print.cutoffs.at=seq(0,1,by=.1), text.adj=c(-0.2,1.7)) #plot ROC curve
+
+#The ideal ROC curve has area-under-the-curve of one, so let's see how close we get to one:
+as.numeric( ROCR::performance(ROCR::prediction(phat, yTest3), "auc")@y.values ) #AUC(ROC)
+#ANSWER: 0.5 TOO.
 
 #############Elastic Net Reg for option2: Make med variables binary#############
 set.seed(777)
@@ -214,6 +267,26 @@ cv.lasso.b4$lambda.1se #The most-regularized model within one standard error of 
 
 #Estimated Odds Ratios
 lasso.b.coef4 <- coef(cv.lasso.b4, s=cv.lasso.b4$lambda.1se)
-lasso.b.coef4#Estimated Odds Ratios
-##~~~~~~~~~Better! 9 VARIABLE LOOKS RIGHT.
+lasso.b.coef4 #####MAJORITY ARE DROPPED!
+#exp(lasso.b.coef4) #Estimated Odds Ratios
+#BEST MODEL?! 9 VARIABLE LOOKS RIGHT.
 
+#~~~~~~ROC Elastic Net Option 2~~~~~~~~
+#FIRST, look at the estimated probabilities, and then convert these into"TRUE" or "FALSE":
+phat <- predict(cv.lasso.b4, newx=xTest4[1:10,], s=cv.lasso.b4$lambda.1se, type="response")
+yhat <- predict(cv.lasso.b4, newx=xTest4[1:10,], s=cv.lasso.b4$lambda.1se, type="class")
+cbind(phat, yhat, yTest4[1:10], unname(dTest4[1:10,]))
+
+#SECOND,consider many ways to convert the estimated probabilities into TRUE or FALSE
+phat <- predict(cv.lasso.b4, newx=xTest4, s=cv.lasso.b4$lambda.1se, type="response")
+#install.packages("ROCR")
+library(ROCR)
+perf <- ROCR::performance(ROCR::prediction(phat, yTest4), "tpr", "fpr")
+par(las=1)
+plot(perf, colorize=TRUE, print.cutoffs.at=seq(0,1,by=.1), text.adj=c(-0.2,1.7)) #plot ROC curve
+
+#The ideal ROC curve has area-under-the-curve of one, so let's see how close we get to one:
+as.numeric( ROCR::performance(ROCR::prediction(phat, yTest4), "auc")@y.values ) #AUC(ROC)
+#ANSWER: 0.7230496
+
+#########The conclusion is that the model seems to have reasonable performance. Ultimately, you would compare AUC(ROC) for several competing models.
