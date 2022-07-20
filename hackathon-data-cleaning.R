36d70b6a1ca48e0ded7d32f7889145f9f0b48d27
LucyL0v0 <yliu268@ncsu.edu>
2022-07-20 12:18:56 -0400

    Update hackathon-data-cleaning.R

--- hackathon-data-cleaning.R
+++ hackathon-data-cleaning.R
@@ -106,7 +106,7 @@ drop1(step_model, test='LRT')
 step_cor <- cor(select(step_data, -REC_IM))
 corrplot(step_cor, type='upper', order='hclust',
          tl.col='black', tl.srt=45, method='square')
-=======
+
 summary(glm(REC_IM ~ AGE + STENOK_AN + endocr_01 + zab_leg_01 +
             GT_POST + lat_im + R_AB_3_n + NA_R_2_n + ANT_CA_S_n +
               GEPAR_S_n + TRENT_S_n, family='binomial', data=mi_comp_clean))
@@ -119,7 +119,6 @@ summary(glm(REC_IM ~ AGE + STENOK_AN + endocr_01 + zab_leg_01 +
 
 #option 2
 #mi_comp_clean2NEW <- mice(mi_comp_clean2, m=10,method = 'pmm', seed = 5)
->>>>>>> 4ba751852eddb75e122b184a810998a14dc545f3
 
 #--------------------LASSO/Elastic Net LOGISTIC REGRESSION----------------------------
 library(caret)
@@ -181,15 +180,15 @@ as.numeric( ROCR::performance(ROCR::prediction(phat, yTest), "auc")@y.values ) #
 #############LASSO Reg for option2: Make med variables binary#############
 set.seed(777)
 #LASSO Reg for option2
-inTrain2 <- as.vector(createDataPartition(mi_comp_clean2[,40],p=0.8,list=FALSE, times=1))
+inTrain2 <- as.vector(createDataPartition(mi_comp_clean2[,46],p=0.8,list=FALSE, times=1))
 dTrain2 <- mi_comp_clean2[inTrain2,]; dim(dTrain2)
 dTest2 <- mi_comp_clean2[-inTrain2,]; dim(dTest2)
 
 #gather response var into vectors
-yTrain2 <- dTrain2[,40]
-xTrain2 <- as.matrix(dTrain2[,-40])
-yTest2 <- dTest2[,40]
-xTest2 <- as.matrix(dTest2[,-40])
+yTrain2 <- dTrain2[,46]
+xTrain2 <- as.matrix(dTrain2[,-46])
+yTest2 <- dTest2[,46]
+xTest2 <- as.matrix(dTest2[,-46])
 
 fit.lasso.b2 <- glmnet(xTrain2, yTrain2, alpha=1, standardize=TRUE, family="binomial")
 plot(fit.lasso.b2, label=TRUE, xvar="lambda")
@@ -204,11 +203,8 @@ cv.lasso.b2$lambda.min #Average mean-squared prediction error is minimized when 
 cv.lasso.b2$lambda.1se #The most-regularized model within one standard error of this "minimum' ' model has lambda = 0.047 and includes 0 predictors
 
 #Estimated Odds Ratios
-<<<<<<< HEAD
-lasso.b.coef2 <- coef(cv.lasso.b2, s=cv.lasso.b2$lambda.1se) #lambda.1se --> lambda.min b/c option 1 ends up with 0 variables.
-=======
 lasso.b.coef2 <- coef(cv.lasso.b2, s=cv.lasso.b2$lambda.1se) #lambda.1se (if Cross Validataion Curve's second vertial line ends up with More than 0 variables) VS. lambda.min --> if Cross Validataion Curve's second vertial line ends up with 0 variables.
->>>>>>> 4ba751852eddb75e122b184a810998a14dc545f3
+
 lasso.b.coef2 ######LESS ARE DROPPED THAN OPTION1
 #exp(lasso.b.coef2) #Estimated Odds Ratios
 #LOOKS BETTER, 5 VARIABLE IS LITTLE LESS
@@ -232,23 +228,23 @@ as.numeric( ROCR::performance(ROCR::prediction(phat, yTest2), "auc")@y.values ) 
 #ANSWER: 0.7223995
 
 ###############Elsatic Net Reg for option1: Reduce dimensionality of ordinal med variables############
-inTrain3 <- as.vector(createDataPartition(mi_comp_clean1[,40],p=0.8,list=FALSE, times=1))
-dTrain3 <- mi_comp_clean1[inTrain3,]; dim(dTrain3)
-dTest3 <- mi_comp_clean1[-inTrain3,]; dim(dTest3)
+#inTrain3 <- as.vector(createDataPartition(mi_comp_clean1[,40],p=0.8,list=FALSE, times=1))
+#dTrain3 <- mi_comp_clean1[inTrain3,]; dim(dTrain3)
+#dTest3 <- mi_comp_clean1[-inTrain3,]; dim(dTest3)
 
 #gather response var into vectors
-yTrain3 <- dTrain3[,40]
-xTrain3 <- as.matrix(dTrain3[,-40])
-yTest3 <- dTest3[,40]
-xTest3 <- as.matrix(dTest3[,-40])
+#yTrain3 <- dTrain3[,40]
+#xTrain3 <- as.matrix(dTrain3[,-40])
+#yTest3 <- dTest3[,40]
+#xTest3 <- as.matrix(dTest3[,-40])
 
 #To fit a Ridge logistic regression model, the only modifications needed are to specify a factor response variable and to include family="binomial" in the glmnet call:
-fit.lasso.b3 <- glmnet(xTrain3, yTrain3, alpha=0.5, standardize=TRUE, family="binomial")
+fit.lasso.b3 <- glmnet(xTrain, yTrain, alpha=0.5, standardize=TRUE, family="binomial")
 plot(fit.lasso.b3, label=TRUE, xvar="lambda")
 
 
 set.seed(777)
-cv.lasso.b3 <- cv.glmnet(xTrain3, yTrain3, alpha=0.5, standardize=TRUE, family="binomial", nfolds=10)
+cv.lasso.b3 <- cv.glmnet(xTrain, yTrain, alpha=0.5, standardize=TRUE, family="binomial", nfolds=10)
 plot(cv.lasso.b3)
 
 cv.lasso.b3$lambda.min #Average mean-squared prediction error is minimized when lambda = 0.01856392; this model includes 4 predictors
@@ -262,40 +258,40 @@ lasso.b.coef3 ###########NO GOOD... EVERYTHING DROPPED.
 
 #~~~~~~ROC Elastic Net Option 1~~~~~~~~
 #FIRST, look at the estimated probabilities, and then convert these into"TRUE" or "FALSE":
-phat <- predict(cv.lasso.b3, newx=xTest3[1:10,], s=cv.lasso.b3$lambda.1se, type="response")
-yhat <- predict(cv.lasso.b3, newx=xTest3[1:10,], s=cv.lasso.b3$lambda.1se, type="class")
+phat <- predict(cv.lasso.b3, newx=xTest[1:10,], s=cv.lasso.b3$lambda.1se, type="response")
+yhat <- predict(cv.lasso.b3, newx=xTest[1:10,], s=cv.lasso.b3$lambda.1se, type="class")
 cbind(phat, yhat, yTest3[1:10], unname(dTest3[1:10,]))
 
 #SECOND,consider many ways to convert the estimated probabilities into TRUE or FALSE
-phat <- predict(cv.lasso.b3, newx=xTest3, s=cv.lasso.b3$lambda.1se, type="response")
+phat <- predict(cv.lasso.b3, newx=xTest, s=cv.lasso.b3$lambda.1se, type="response")
 #install.packages("ROCR")
 library(ROCR)
-perf <- ROCR::performance(ROCR::prediction(phat, yTest3), "tpr", "fpr")
+perf <- ROCR::performance(ROCR::prediction(phat, yTest), "tpr", "fpr")
 par(las=1)
 plot(perf, colorize=TRUE, print.cutoffs.at=seq(0,1,by=.1), text.adj=c(-0.2,1.7)) #plot ROC curve
 
 #The ideal ROC curve has area-under-the-curve of one, so let's see how close we get to one:
-as.numeric( ROCR::performance(ROCR::prediction(phat, yTest3), "auc")@y.values ) #AUC(ROC)
+as.numeric( ROCR::performance(ROCR::prediction(phat, yTest), "auc")@y.values ) #AUC(ROC)
 #ANSWER: 0.5 TOO.
 
 #############Elastic Net Reg for option2: Make med variables binary#############
 set.seed(777)
 #LASSO Reg for option2
-inTrain4 <- as.vector(createDataPartition(mi_comp_clean2[,40],p=0.8,list=FALSE, times=1))
-dTrain4 <- mi_comp_clean2[inTrain4,]; dim(dTrain2)
-dTest4 <- mi_comp_clean2[-inTrain4,]; dim(dTest2)
+#inTrain4 <- as.vector(createDataPartition(mi_comp_clean2[,40],p=0.8,list=FALSE, times=1))
+#dTrain4 <- mi_comp_clean2[inTrain4,]; dim(dTrain2)
+#dTest4 <- mi_comp_clean2[-inTrain4,]; dim(dTest2)
 
 #gather response var into vectors
-yTrain4 <- dTrain4[,40]
-xTrain4 <- as.matrix(dTrain4[,-40])
-yTest4 <- dTest4[,40]
-xTest4 <- as.matrix(dTest4[,-40])
+#yTrain4 <- dTrain4[,40]
+#xTrain4 <- as.matrix(dTrain4[,-40])
+#yTest4 <- dTest4[,40]
+#xTest4 <- as.matrix(dTest4[,-40])
 
-fit.lasso.b4 <- glmnet(xTrain4, yTrain4, alpha=0.5, standardize=TRUE, family="binomial")
+fit.lasso.b4 <- glmnet(xTrain2, yTrain2, alpha=0.5, standardize=TRUE, family="binomial")
 plot(fit.lasso.b4, label=TRUE, xvar="lambda")
 
 set.seed(3201)
-cv.lasso.b4 <- cv.glmnet(xTrain4, yTrain4, alpha=0.5, standardize=TRUE, family="binomial", nfolds=10)
+cv.lasso.b4 <- cv.glmnet(xTrain2, yTrain2, alpha=0.5, standardize=TRUE, family="binomial", nfolds=10)
 plot(cv.lasso.b4)
 
 #Estimating Odds Ratio
@@ -311,20 +307,20 @@ lasso.b.coef4 #####MAJORITY ARE DROPPED!
 
 #~~~~~~ROC Elastic Net Option 2~~~~~~~~
 #FIRST, look at the estimated probabilities, and then convert these into"TRUE" or "FALSE":
-phat <- predict(cv.lasso.b4, newx=xTest4[1:10,], s=cv.lasso.b4$lambda.1se, type="response")
-yhat <- predict(cv.lasso.b4, newx=xTest4[1:10,], s=cv.lasso.b4$lambda.1se, type="class")
-cbind(phat, yhat, yTest4[1:10], unname(dTest4[1:10,]))
+phat <- predict(cv.lasso.b4, newx=xTest2[1:10,], s=cv.lasso.b4$lambda.1se, type="response")
+yhat <- predict(cv.lasso.b4, newx=xTest2[1:10,], s=cv.lasso.b4$lambda.1se, type="class")
+cbind(phat, yhat, yTest2[1:10], unname(dTest2[1:10,]))
 
 #SECOND,consider many ways to convert the estimated probabilities into TRUE or FALSE
-phat <- predict(cv.lasso.b4, newx=xTest4, s=cv.lasso.b4$lambda.1se, type="response")
+phat <- predict(cv.lasso.b4, newx=xTest2, s=cv.lasso.b4$lambda.1se, type="response")
 #install.packages("ROCR")
 library(ROCR)
-perf <- ROCR::performance(ROCR::prediction(phat, yTest4), "tpr", "fpr")
+perf <- ROCR::performance(ROCR::prediction(phat, yTest2), "tpr", "fpr")
 par(las=1)
 plot(perf, colorize=TRUE, print.cutoffs.at=seq(0,1,by=.1), text.adj=c(-0.2,1.7)) #plot ROC curve
 
 #The ideal ROC curve has area-under-the-curve of one, so let's see how close we get to one:
-as.numeric( ROCR::performance(ROCR::prediction(phat, yTest4), "auc")@y.values ) #AUC(ROC)
+as.numeric( ROCR::performance(ROCR::prediction(phat, yTest2), "auc")@y.values ) #AUC(ROC)
 #ANSWER: 0.7230496
 
 #########The conclusion is that the model seems to have reasonable performance. Ultimately, you would compare AUC(ROC) for several competing models.
